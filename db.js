1adabd609ea973c7515d86036bf8a63f44955713
Debashis Goswami <145210990+Bittu3487@users.noreply.github.com>
2026-04-07 21:03:19 +0530

    deployement

--- db.js
+++ db.js
@@ -14,11 +14,11 @@ if (process.env.DATABASE_URL) {
 } else {
     // ✅ Local PostgreSQL
     pool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
-        port: process.env.DB_PORT,
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.NAME,
+        password: process.env.PASS,
+        port: process.env.PORT,
     });
 }
 
