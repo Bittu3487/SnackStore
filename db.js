d5da964464ce1cb12f732bb49927a6e2c97efd9d
Debashis Goswami <145210990+Bittu3487@users.noreply.github.com>
2026-04-09 08:10:55 +0530

    deployemnt

--- db.js
+++ db.js
@@ -1,14 +1,22 @@
 const { Pool } = require('pg');
-const env = require('dotenv');
-env.config();
 
-    const pool = new Pool({
+let pool;
+
+if (process.env.DATABASE_URL) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    pool = new Pool({
         user: process.env.USER,
         host: process.env.HOST,
         database: process.env.DATABASE,
         password: process.env.PASSWORD,
         port: process.env.PORT,
     });
-
+}
 
 module.exports = pool;
